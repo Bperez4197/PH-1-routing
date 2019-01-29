55e1ca3e0163594e43ed7b72197cbc70c1ac73de
Bryce Perez <bryceperez31@gmail.com>
2019-01-29 12:06:01 -0700

    doing routes and switches together

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function About(props) {
   return (
     <div>
       <h1>This is the About component</h1>
+      <Link to="/about/faq">
       <h3>FAQ</h3>
+      </Link>
+      <Link to="/about/company">
       <h3>Company</h3>
+      </Link>
       {props.children}
     </div>
   )
--- src/components/People/People.js
+++ src/components/People/People.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { getPeople } from '../../usersService.js';
+import {Link} from 'react-router-dom';
+
 
 export default class People extends Component {
   constructor() {
@@ -20,7 +22,9 @@ export default class People extends Component {
   render() {
     const people = this.state.people.map((e, i) => {
       return (
+        <Link key={e.id} to={`/people/${e.id}`}>
         <h3>{e.name}</h3>
+        </Link>
       )
     })
     return (
